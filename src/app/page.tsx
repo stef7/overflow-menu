0a57f46da8d476284398fce32fbbf8545cd620ac
stef7 <1480673+stef7@users.noreply.github.com>
2023-12-10 18:58:27 +1100

    controls to test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,43 @@
+import Link from "next/link";
 import { OverflowMenu } from "./components/OverflowMenu";
 
 const between = (from: number, to: number) => Math.floor(Math.random() * (to + 1 - from) + from);
 
-export default function HomePage() {
-  const rands = [...Array([14, 20, 21, 22, 26][between(0, 4)])].map(() =>
-    [...Array(between(1, 3))].map(() => Math.random().toString(36).slice(-between(2, 5))).join(" ")
+const getRands = (length: number) =>
+  [...Array(length)].map(
+    (_, i) =>
+      `${i + 1}: ${[...Array(between(1, 3))].map(() => Math.random().toString(36).slice(-between(1, 4))).join(" ")}`
   );
 
+export default function HomePage({
+  searchParams,
+}: {
+  readonly searchParams: { [key: string]: string | string[] | undefined };
+}) {
+  const currentLength = Number(searchParams.length) || 10;
+
   return (
-    <OverflowMenu overflowControlLabel="More ▼" className="[&_:where(button,a)]:p-3 [&_:where(button,a)]:block">
-      {rands.map((rand, i) => {
-        return (
+    <>
+      <p className="flex flex-wrap p-4">
+        Length:
+        {[10, 20, 21, 32].map((length) => (
+          <Link
+            key={length}
+            href={{ query: { length } }}
+            className={`p-4 -my-4 ${currentLength === length ? "bg-yellow-200" : ""}`}
+          >
+            {length}
+          </Link>
+        ))}
+      </p>
+
+      <OverflowMenu overflowControlLabel="More ▼" className="[&_:where(button,a)]:p-4 [&_:where(button,a)]:block">
+        {getRands(currentLength).map((rand) => (
           <li key={rand}>
-            <a href={`#${rand}`}>
-              {i + 1}: {rand}
-            </a>
+            <a href={`#${rand}`}>{rand}</a>
           </li>
-        );
-      })}
-    </OverflowMenu>
+        ))}
+      </OverflowMenu>
+    </>
   );
 }
