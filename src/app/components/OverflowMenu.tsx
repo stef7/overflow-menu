d5a82ec9e47a02854a9384462eb721338efd5774
stef7 <1480673+stef7@users.noreply.github.com>
2023-12-10 21:57:25 +1100

    single effect

--- src/app/components/OverflowMenu.tsx
+++ src/app/components/OverflowMenu.tsx
@@ -1,58 +1,50 @@
 "use client";
 
-import React, { forwardRef, useLayoutEffect, useMemo, useState } from "react";
+import React, { forwardRef, useLayoutEffect, useState } from "react";
 
 import styles from "./OverflowMenu.module.scss";
 
 export const OverflowMenu = forwardRef<
   HTMLDivElement,
-  React.HTMLAttributes<HTMLDivElement> & {
-    children: (React.ReactElement | null | undefined)[];
-    overflowControlLabel: string;
-  }
+  React.PropsWithChildren<React.HTMLAttributes<HTMLDivElement> & { overflowControlLabel: string }>
 >(function OverflowMenu({ children, className, overflowControlLabel, ...rest }, ref) {
   const [observedList, setObservedList] = useState<HTMLOListElement | null>(null);
   const [overflowIndexes, setOverflowIndexes] = useState<number[]>();
 
-  const intersectionOb = useMemo(() => {
+  useLayoutEffect(() => {
     if (!observedList) return;
-    return new IntersectionObserver(
+
+    const intersectionOb = new IntersectionObserver(
       (entries) =>
         entries.forEach(({ target, target: { parentElement }, isIntersecting: isIn }) => {
           const kids = Array.from(parentElement?.children ?? []);
           const index = kids.indexOf(target) + 1;
           setOverflowIndexes((prev) =>
-            (prev?.filter((n) => n < kids.length && n !== index) ?? []).concat(isIn ? [] : index)
+            (prev?.filter((n) => n <= kids.length && n !== index) ?? []).concat(isIn || index < 0 ? [] : index)
           );
         }),
       { root: observedList, threshold: 1 }
     );
-  }, [observedList]);
 
-  const mutationOb = useMemo(() => {
-    if (!intersectionOb) return;
-    return new MutationObserver((mutations) =>
+    const mutationOb = new MutationObserver((mutations) =>
       mutations.forEach((m) => {
         m.addedNodes.forEach((node) => node instanceof Element && intersectionOb.observe(node));
         m.removedNodes.forEach((node) => node instanceof Element && intersectionOb.unobserve(node));
       })
     );
-  }, [intersectionOb]);
 
-  useLayoutEffect(() => {
-    if (!observedList || !intersectionOb || !mutationOb) return;
     Array.from(observedList.children).forEach((kid) => intersectionOb.observe(kid));
     mutationOb.observe(observedList, { childList: true });
     return () => {
       mutationOb.disconnect();
       intersectionOb.disconnect();
     };
-  }, [observedList, intersectionOb, mutationOb]);
+  }, [observedList]);
 
   return (
     <div
       {...rest}
-      className={[styles.base, ...(className ? [className] : [])].join(" ")}
+      className={[styles.base, className ?? []].flat().join(" ")}
       data-overflows={overflowIndexes?.join(" ")}
       ref={ref}
     >
